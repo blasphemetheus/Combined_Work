432afa1e2ebe731701f121f11729080d760b0949
Bradley Fargo <blewfargs@gmail.com>
2017-03-20 15:01:08 -0400

    commented out everything but render
    Those other commented out methods might be useful in the implementation of specific views but as far as I can tell, all a view NEEDS to do is render itself somehow.

--- src/cs3500/music/view/ViewOperations.java
+++ src/cs3500/music/view/ViewOperations.java
@@ -8,63 +8,67 @@ import java.awt.event.KeyListener;
  */
 public interface ViewOperations {
 
-
   /**
-   * Set the label that is showing what the model stores.
-   */
-  void setEchoOutput(String s);
-
-  /**
-   * Get the string from the ext field and return it.
-   * @return the String from the text field
-   */
-  String getInputString();
-
-
-  /**
-   * Clears the text field. (not set input string because the user sets it with input).
-   */
-  void clearInputString();
-
-  /**
-   * Resets the focus back to the frame.
-   */
-  void resetFocus();
-
-  /**
-   * Toggle the color of the displayed text. Explicitly in the view's domain.
-   */
-  void toggleColor();
-
-  /**
-   * Forces the view to have a method to set up the (typing) keyboard.
-   * Same method signature to add a KeyListener to Swing.
-   *
-   * @param listener the listener to be added
-   */
-  void addKeyListener(KeyListener listener);
-
-
-  /**
-   * Forces the view to have a method to set up actions for buttons.
-   * All buttons must be given this action listener.
-   *
-   * @param listener the listener to be added
-   */
-  void addActionListener(ActionListener listener);
-
-  // Optional ones that might be fun
-  /**
-   * Replaces the song title with the given String.
-   * @param newTitle the new title to be set
-   */
-  void setSongTitle(String newTitle);
-
-
-  /**
-   * Renders the view (for textual
+   * Renders the view, whatever the specifics of that rendering may be.
    */
   void render();
 
-
 }
+
+//  // THIS IS ALL STUFF THAT WOULD BE NICE OR IS A FOSSIL FROM THE MVC EXAMPLE
+//
+//  /**
+//   * Sets the tempo to some bpm (only accepts positive non-zero numbers with some upper limit).
+//   */
+//  void setTempo();
+//
+//  /**
+//   * Set the label that is showing what the model stores.
+//   */
+//  void setEchoOutput(String s);
+//
+//  /**
+//   * Get the string from the ext field and return it.
+//   * @return the String from the text field
+//   */
+//  String getInputString();
+//
+//
+//  /**
+//   * Clears the text field. (not set input string because the user sets it with input).
+//   */
+//  void clearInputString();
+//
+//  /**
+//   * Resets the focus back to the frame.
+//   */
+//  void resetFocus();
+//
+//  /**
+//   * Toggle the color of the displayed text. Explicitly in the view's domain.
+//   */
+//  void toggleColor();
+//
+//  /**
+//   * Forces the view to have a method to set up the (typing) keyboard.
+//   * Same method signature to add a KeyListener to Swing.
+//   *
+//   * @param listener the listener to be added
+//   */
+//  void addKeyListener(KeyListener listener);
+//
+//
+//  /**
+//   * Forces the view to have a method to set up actions for buttons.
+//   * All buttons must be given this action listener.
+//   *
+//   * @param listener the listener to be added
+//   */
+//  void addActionListener(ActionListener listener);
+//
+//  // Optional ones that might be fun
+//  /**
+//   * Replaces the song title with the given String.
+//   * @param newTitle the new title to be set
+//   */
+//  void setSongTitle(String newTitle);
