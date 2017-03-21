fe84b55856f1b3a568200f4e8d5ca8b304ca6651
Asad Ali Khan <khan.asa@husky.neu.edu>
2017-03-20 23:30:19 -0400

    Update TextualView.java

--- src/cs3500/music/view/TextualView.java
+++ src/cs3500/music/view/TextualView.java
@@ -4,14 +4,35 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import cs3500.music.model.Duration;
+import cs3500.music.model.MusicModel;
 import cs3500.music.model.Note;
+import cs3500.music.model.Octave;
+import cs3500.music.model.Pitch;
 
 /**
  * Created by blewf on 3/18/2017.
  */
 public class TextualView implements ViewOperations {
 
+
   // MAKE SURE TO USE getNotes() !!!!!!!!!!!!!!
+  /**
+   * Helper method to configure the number of columns.
+   * If you're on the last octave then print the last possible pitch
+   * otherwise print all possible pitches till the next octave.
+   *
+   * @param i   the octave to be printed
+   * @param max the max value a note can be according to its pitch
+   * @return number of columns
+   */
+  protected int columns(int i, int max) {
+    if (i == max) {
+      return max;
+    } else {
+      return 11;
+    }
+  }
 
   /**
    * Returns the state of the Model as a String (in columnar form) <p>Technical Specs: - A column of
@@ -31,38 +52,42 @@ public class TextualView implements ViewOperations {
    *
    * @return the state of the game
    */
-  String getState();
-
-  @Override
-  public String getMusicState() {
+//  String getState();
+  public String getMusicState(MusicModel model) {
 
     String musicState = "";
-    if (Music.isEmpty()) {
+    if (model.getNotes().isEmpty()) {
       return musicState;
     }
     ///Find end point of song(last note endAt beat)
     ///to set the required space for beats column
     ///We need this now to know where to start our first line
     List<Integer> beats = new ArrayList<>();
-    for (int i = 0; i < Music.size(); i++) {
-      beats.add(Music.get(i).getDur().endAt);
+    for (int i = 0; i < model.getNotes().size(); i++) {
+      List<Note> lon = model.getNotes();
+      Duration dur = lon.get(i).getDur();
+      int endBeat = dur.getEndBeat();
+      beats.add(endBeat);
     }
     ///Add necessary gaps to first line
     int beatMax = Collections.max(beats);
+    // bmlength is the number of digits
     int bmLength = String.valueOf(beatMax).length();
-    for (int i = 0; i < bmLength - 2; i++) {
+
+    for (int i = 0; i < bmLength; i++) {
       musicState += " ";
     }
     ///Accumulate pitches and octaves to empty lists to determine min and max values
     List<Integer> firstline1 = new ArrayList<>();
     List<Integer> firstline2 = new ArrayList<>();
     List<Note> firstline = new ArrayList<>();
-    for (int i = 0; i < Music.size(); i++) {
-      firstline1.add(Music.get(i).pitch);
+    for (int i = 0; i < model.getNotes().size(); i++) {
+      firstline1.add(model.getNotes().get(i).getPitch().getOrder());
     }
-    for (int i = 0; i < Music.size(); i++) {
-      firstline2.add(Music.get(i).octave);
+    for (int i = 0; i < model.getNotes().size(); i++) {
+      firstline2.add(model.getNotes().get(i).getOctave().toInt());
     }
+    ///Sort out maximums and minimums
     int pitchMin = Collections.min(firstline1);
     int pitchMax = Collections.max(firstline1);
     int octaveMin = Collections.min(firstline2);
@@ -72,7 +97,9 @@ public class TextualView implements ViewOperations {
     ///to an empty list
     for (int i = octaveMin; i <= octaveMax; i++) {
       for (int j = pitchMin; j <= columns(i, pitchMax); j++) {
-        firstline.add(new Note(j, i, null));
+        // firstline.add(new Note(new Pitch(j, null), new Octave(i), null));
+        firstline.add(new Note(Pitch.getPint(j), Octave.fromInt(i), null));
+
       }
     }
 
@@ -102,27 +129,27 @@ public class TextualView implements ViewOperations {
       ///loop through 5 character spaces
       for (int j = 0; j < firstline.size(); j++) {
         ///loop through list for possible X or | or empty
-        for (int k = 0; k < Music.size(); k++) {
-          if (Music.get(k).getDur().startAt == i
-                  && Music.get(k).pitch == firstline.get(j).pitch
-                  && Music.get(k).octave == firstline.get(j).octave) {
+        for (int k = 0; k < model.getNotes().size(); k++) {
+          if (model.getNotes().get(k).getDur().getStartBeat() == i
+                  && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
+                  && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
             musicState += "  X  ";
           } else {
-            if (Music.get(k).getDur().endAt == i
-                    && Music.get(k).pitch == firstline.get(j).pitch
-                    && Music.get(k).octave == firstline.get(j).octave) {
+            if (model.getNotes().get(k).getDur().getEndBeat() == i
+                    && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
+                    && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
               musicState += "  |  ";
             } else {
-              if (Music.get(k).getDur().startAt < i
-                      && Music.get(k).getDur().endAt >= i
-                      && Music.get(k).pitch == firstline.get(j).pitch
-                      && Music.get(k).octave == firstline.get(j).octave) {
+              if (model.getNotes().get(k).getDur().getStartBeat() < i
+                      && model.getNotes().get(k).getDur().getEndBeat() >= i
+                      && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
+                      && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
                 musicState += "  |  ";
               }
             }
           }
         }
-        if (!(Music.contains(firstline.get(j)))) {
+        if (!(model.getNotes().contains(firstline.get(j)))) {
           musicState += "     ";
         }
       }
@@ -133,39 +160,43 @@ public class TextualView implements ViewOperations {
   }
 
 
+//  @Override
+//  public String getState() {
+//    String output = "";
+//    Note lowestNote = this.getLowestNote();
+//    Note highestNote = this.getHighestNote();
+//    List<Note> encountered = new ArrayList<>();
+//    List<Integer> encounteredBeats = new ArrayList<>();
+//
+//    // DEALS WITH FIRST LINE -- SHOULD GENERATE
+//    // if empty, lowestNote and highestNote should return null values
+//    if (this.isEmpty()) {
+//      output.concat(this.firstLine(null, null));
+//    } else {
+//      output.concat(this.firstLine(lowestNote, highestNote));
+//      // if this branch ever returns null lowestnote and null highestNote
+//      // then I screwed up in getLowestNote() and getHighestNote();
+//    }
+//
+//    int totalBeats = this.numBeats();
+//    for (int currentBeat = 0; currentBeat < totalBeats; currentBeat += 1) {
+//      if (this.map.containsKey(currentBeat)) {
+//        for (Note tempNote : this.map.get(currentBeat)) {
+//          encountered.add(tempNote);
+//        }
+//        encountered.addAll(this.map.get(currentBeat));
+//        encounteredBeats.add(currentBeat);
+//      }
+//
+//      // the intro to every line but the first
+//      output.concat(this.getPaddedInt(currentBeat, totalBeats));
+//      output.concat(this.genPerBeat(currentBeat, lowestNote, highestNote, encountered));
+//    }
+//    return output;
+//  }
+//
   @Override
-  public String getState() {
-    String output = "";
-    Note lowestNote = this.getLowestNote();
-    Note highestNote = this.getHighestNote();
-    List<Note> encountered = new ArrayList<>();
-    List<Integer> encounteredBeats = new ArrayList<>();
+  public void render() {
 
-    // DEALS WITH FIRST LINE -- SHOULD GENERATE
-    // if empty, lowestNote and highestNote should return null values
-    if (this.isEmpty()) {
-      output.concat(this.firstLine(null, null));
-    } else {
-      output.concat(this.firstLine(lowestNote, highestNote));
-      // if this branch ever returns null lowestnote and null highestNote
-      // then I screwed up in getLowestNote() and getHighestNote();
-    }
-
-    int totalBeats = this.numBeats();
-    for (int currentBeat = 0; currentBeat < totalBeats; currentBeat += 1) {
-      if (this.map.containsKey(currentBeat)) {
-        for (Note tempNote : this.map.get(currentBeat)) {
-          encountered.add(tempNote);
-        }
-        encountered.addAll(this.map.get(currentBeat));
-        encounteredBeats.add(currentBeat);
-      }
-
-      // the intro to every line but the first
-      output.concat(this.getPaddedInt(currentBeat, totalBeats));
-      output.concat(this.genPerBeat(currentBeat, lowestNote, highestNote, encountered));
-    }
-    return output;
   }
-
 }
