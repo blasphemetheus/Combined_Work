cfe9e7f8f9fb6feb34eed4cc7a7729dfea341c47
Bradley Fargo <blewfargs@gmail.com>
2017-03-22 09:39:43 -0400

    cut down considerably

--- src/cs3500/music/view/ViewOperations.java
+++ src/cs3500/music/view/ViewOperations.java
@@ -1,7 +1,7 @@
 package cs3500.music.view;
 
-import java.awt.event.ActionListener;
-import java.awt.event.KeyListener;
+import cs3500.music.controller.ButtonListener;
+import cs3500.music.controller.KeyboardListener;
 
 /**
  * The operations that all of my views need to be able to do.
@@ -13,62 +13,20 @@ public interface ViewOperations {
    */
   void render();
 
-}
 
-//  // THIS IS ALL STUFF THAT WOULD BE NICE OR IS A FOSSIL FROM THE MVC EXAMPLE
-//
-//  /**
-//   * Sets the tempo to some bpm (only accepts positive non-zero numbers with some upper limit).
-//   */
-//  void setTempo();
-//
-//  /**
-//   * Set the label that is showing what the model stores.
-//   */
-//  void setEchoOutput(String s);
-//
-//  /**
-//   * Get the string from the ext field and return it.
-//   * @return the String from the text field
-//   */
-//  String getInputString();
-//
-//
-//  /**
-//   * Clears the text field. (not set input string because the user sets it with input).
-//   */
-//  void clearInputString();
-//
-//  /**
-//   * Resets the focus back to the frame.
-//   */
-//  void resetFocus();
-//
-//  /**
-//   * Toggle the color of the displayed text. Explicitly in the view's domain.
-//   */
-//  void toggleColor();
-//
-//  /**
-//   * Forces the view to have a method to set up the (typing) keyboard.
-//   * Same method signature to add a KeyListener to Swing.
-//   *
-//   * @param listener the listener to be added
-//   */
-//  void addKeyListener(KeyListener listener);
-//
-//
-//  /**
-//   * Forces the view to have a method to set up actions for buttons.
-//   * All buttons must be given this action listener.
-//   *
-//   * @param listener the listener to be added
-//   */
-//  void addActionListener(ActionListener listener);
-//
-//  // Optional ones that might be fun
-//  /**
-//   * Replaces the song title with the given String.
-//   * @param newTitle the new title to be set
-//   */
-//  void setSongTitle(String newTitle);
+    /**
+   * Forces the view to have a method to set up the (typing) keyboard.
+   * Same method signature to add a KeyListener to Swing.
+   *
+   * @param kbd the listener to be added
+   */
+  void addKeyListener(KeyboardListener kbd);
+
+  /**
+   * Forces the view to have a method to set up actions for buttons.
+   * All buttons must be given this action listener.
+   *
+   * @param buttonListener the listener to be added
+   */
+  void addActionListener(ButtonListener buttonListener);
+}
