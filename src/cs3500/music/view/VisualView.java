fd05ba95fa6f013db5164ce088cc2df863bf6284
Asad Ali Khan <khan.asa@husky.neu.edu>
2017-04-05 20:13:40 -0400

    Update VisualView.java

--- src/cs3500/music/view/VisualView.java
+++ src/cs3500/music/view/VisualView.java
@@ -1,8 +1,10 @@
 package cs3500.music.view;
 
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
+
+import javax.swing.*;
 
 import cs3500.music.model.Duration;
 import cs3500.music.model.MusicModel;
@@ -11,82 +13,179 @@ import cs3500.music.model.Octave;
 import cs3500.music.model.Pitch;
 
 /**
- * Created by blewf on 3/18/2017.
+ * A dummy view that simply draws a string 
  */
-public class TextualView implements ViewOperations {
+public class ConcreteGuiViewPanel extends JPanel {
+
+  private MusicModel model;
+
+  public ConcreteGuiViewPanel(MusicModel model) {
+    this.model = model;
+  }
+  public final int NOTE_HEIGHT = 10;
+  public final int NOTE_WIDTH = 20;
+  public final int PIANO_WIDTH = 20;
+  public final int PIANO_HEIGHT = 250;
+
+  public java.util.List<Note> firstline = new ArrayList<>();
+  public java.util.List<Integer> beats = new ArrayList<>();
+  public java.util.List<Note> allNotes = new ArrayList<>();
 
 
-  // MAKE SURE TO USE getNotes() !!!!!!!!!!!!!!
+
+  @Override
+  public void paintComponent(Graphics g){
+    // Handle the default painting
+    super.paintComponent(g);
+    g.clearRect(0,0,WIDTH,HEIGHT);
+    // Look for more documentation about the Graphics class,
+    // and methods on it that may be useful
+    makeNoteText(g);
+    makeBeatText(g);
+    makeRedLine(g);
+    makeNotes(g);
+    makeNoteGrid(g);
+    makePiano(g);
+  }
+
   /**
-   * Helper method to configure the number of columns.
-   * If you're on the last octave then print the last possible pitch
-   * otherwise print all possible pitches till the next octave.
-   *
-   * @param i   the octave to be printed
-   * @param max the max value a note can be according to its pitch
-   * @return number of columns
+   * Draw the piano tiles: make a white tile if it is flat otherwise make a black one for sharp
+   * @param g
    */
-  public int columns(int i, int max) {
-    if (i == max) {
-      return max;
-    } else {
-      return 11;
+  public void makePiano(Graphics g) {
+
+    ///make all possible combination of notes for piano keys
+    for (int i = 1; i <= 10; i++) {
+      for (int j = 1; j <= columns(i, 12); j++) {
+        // firstline.add(new Note(new Pitch(j, null), new Octave(i), null));
+        allNotes.add(new Note(Pitch.getPint(j), Octave.fromInt(i), null));
+      }
+    }
+    for(int i = 0; i < allNotes.size(); i++) {
+      if (!(allNotes.get(i).getPitch().toString().contains("#"))) {
+        g.drawRect(i * PIANO_WIDTH,(NOTE_WIDTH/2 + NOTE_WIDTH) + NOTE_WIDTH * firstline
+                        .size(),
+                PIANO_WIDTH * 2,
+                PIANO_HEIGHT);
+      } else {
+        if (allNotes.get(k).getPitch().toString().contains("#")) {
+          g.fillRect(k * PIANO_WIDTH, (NOTE_WIDTH / 2 + NOTE_WIDTH) +
+                  NOTE_WIDTH * firstline.size(), PIANO_WIDTH / 2, PIANO_HEIGHT / 2);
+          allNotes.remove(k);
+          k--;
+        }
+      }
     }
   }
 
   /**
-   * Returns the state of the Model as a String (in columnar form) <p>Technical Specs: - A column of
-   * numbers representing the beats, printed right-justified and padded with leading spaces, that is
-   * exactly as wide as necessary. (So if your piece is 999 beats long, it uses three columns of
-   * characters; if it’s 1000 beats long, it uses four.) - A sequence of columns, each five
-   * characters wide, representing each pitch. The first line prints out the names of the pitches,
-   * more-or-less centered within the five-character column. I.e., "  F2 " and " G#3 " and " D#10".
-   * Because we need to represent at least ten octaves, three-character columns won’t be wide
-   * enough.) - Use exactly as many columns as are needed for your piece, from its lowest to its
-   * highest note. - Each note-head is rendered as an "  X  ", and each note-sustain is rendered as
-   * "  |  ". When a note is not played, five spaces are rendered (as "     "). - As a consequence:
-   * every line should be exactly the same length, as shown above. - Every item, including the last
-   * one, ends in a newline. - Use the # character (the standard hash or pound sign) to represent
-   * sharps, rather than the more correct ♯, to avoid any formatting errors when running your
-   * code.</p>
-   *
-   * @return the state of the game
+   * Draws a note onto the grid depending on the state of the note. A note which is not being
+   * played wil be blank. A note that is in its onset will be black. A not being played but not
+   * in its onset will be green.
+   * @param g
    */
-//  String getState();
-  public String getMusicState(MusicModel model) {
-
-    String musicState = "";
-    if (model.getNotes().isEmpty()) {
-      return musicState;
+  public void makeNotes(Graphics g) {
+    int duration;
+    for (Note note : model.getNotes()) {
+      duration = note.getDur().getEndBeat() - note.getBeatStart();
+      g.setColor(Color.GREEN);
+      g.fillRect(note.getBeatStart() * NOTE_WIDTH + (2 * NOTE_WIDTH),
+              makeY(note),
+               //(Collections.max(beats) - note.getPitch().getOrder()),
+              NOTE_WIDTH * (duration),
+              NOTE_HEIGHT * 2);
+      g.setColor(Color.BLACK);
+      g.fillRect(note.getBeatStart() * NOTE_WIDTH + (2 * NOTE_WIDTH),
+              //(Collections.max(beats) - note.getPitch().getOrder()),
+              makeY(note),
+              NOTE_WIDTH,
+              NOTE_HEIGHT * 2);
     }
-    ///Find end point of song(last note endAt beat)
-    ///to set the required space for beats column
-    ///We need this now to know where to start our first line
-    List<Integer> beats = new ArrayList<>();
+  }
+   /**
+   * Sets the Y posns of the notes according to their Notes. 
+   * 
+   * @param note
+   * @return Y posn for notes
+   */
+  public int makeY(Note note) {
+    int k = 0;
+    for(int i=0;i<firstline.size();i++) {
+      if (note.getPitch().getOrder() == firstline.get(i).getPitch().getOrder()) {
+        k = (3 * NOTE_HEIGHT - NOTE_HEIGHT/2) +
+                NOTE_WIDTH * i;
+      }
+    }
+    return k;
+  }
+  /**
+   * Draw the grid that will hold all the notes. Every change in octave will be indicated by a
+   * bolder lone
+   * @param g
+   */
+  public void makeNoteGrid(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    g.setColor(Color.black);
+    g2.setStroke(new BasicStroke(2));
+    for (int i = 0; i < firstline.size(); i++) {
+      for (int j = 0; j <= Collections.max(beats)/4; j++) {
+        g.drawRect(j * (4 * NOTE_WIDTH) + (NOTE_HEIGHT * 4), (3 * NOTE_HEIGHT - NOTE_HEIGHT/2) +
+                        NOTE_WIDTH * i,
+                NOTE_WIDTH * 4,
+                NOTE_HEIGHT * 2);
+      }
+    }
+  }
+
+  /**
+   * Draw the red line that tracks the beat being played. THe redline only exists within the grid
+   * that holds all the notes.
+   * @param g
+   */
+  public void makeRedLine(Graphics g) {
+    Graphics2D g2 = (Graphics2D) g;
+    g2.setColor(Color.RED);
+    g2.setStroke(new BasicStroke(4));
+    g2.drawLine(NOTE_HEIGHT * 4,2 * NOTE_WIDTH - 5 ,NOTE_HEIGHT * 4,firstline.size() *
+            NOTE_WIDTH + NOTE_WIDTH);
+  }
+
+  /**
+   * Draws the beats in increments of four only to end at the last possible beat(max beat)
+   * @param g
+   */
+  public void makeBeatText(Graphics g) {
+    ///Extract the end beats of our notes and store them
     for (int i = 0; i < model.getNotes().size(); i++) {
-      List<Note> lon = model.getNotes();
+      java.util.List<Note> lon = model.getNotes();
       Duration dur = lon.get(i).getDur();
       int endBeat = dur.getEndBeat();
       beats.add(endBeat);
     }
-    ///Add necessary gaps to first line
+    ///Get the last beat
     int beatMax = Collections.max(beats);
-    // bmlength is the number of digits
-    int bmLength = String.valueOf(beatMax).length();
 
-    for (int i = 0; i < bmLength; i++) {
-      musicState += " ";
+    for(int i = 0; i <= beatMax; i += 4) {
+      g.drawString(i + "", (i * NOTE_WIDTH) + (NOTE_HEIGHT * 4) ,NOTE_WIDTH);
     }
+  }
+
+  /**
+   * Draws all the Pitches and Octaves that are within the highest and lowest note on the left side
+   * of the panel.
+   * @param g
+   */
+  public void makeNoteText(Graphics g) {
     ///Accumulate pitches and octaves to empty lists to determine min and max values
-    List<Integer> firstline1 = new ArrayList<>();
-    List<Integer> firstline2 = new ArrayList<>();
-    List<Note> firstline = new ArrayList<>();
+    java.util.List<Integer> firstline1 = new ArrayList<>();
+    java.util.List<Integer> firstline2 = new ArrayList<>();
     for (int i = 0; i < model.getNotes().size(); i++) {
       firstline1.add(model.getNotes().get(i).getPitch().getOrder());
     }
     for (int i = 0; i < model.getNotes().size(); i++) {
       firstline2.add(model.getNotes().get(i).getOctave().toInt());
     }
+
     ///Sort out maximums and minimums
     int pitchMin = Collections.min(firstline1);
     int pitchMax = Collections.max(firstline1);
@@ -99,103 +198,33 @@ public class TextualView implements ViewOperations {
       for (int j = pitchMin; j <= columns(i, pitchMax); j++) {
         // firstline.add(new Note(new Pitch(j, null), new Octave(i), null));
         firstline.add(new Note(Pitch.getPint(j), Octave.fromInt(i), null));
-      }
-    }
 
-    ///loop the list with the correct character spaces between them(5)
-    for (int k = 0; k < firstline.size(); k++) {
-      switch (firstline.get(k).toString().length()) {
-        case 2:
-          musicState += "  " + firstline.get(k).toString() + " ";
-          break;
-        case 3:
-          musicState += " " + firstline.get(k).toString() + " ";
-          break;
-        case 4:
-          musicState += " " + firstline.get(k).toString() + "";
-          break;
-        default:
-          musicState += "" + firstline.get(k).toString() + "";
       }
     }
-    musicState += "\n";
-
-    ///Now each corresponding line:
-    ///Print each line vertically
-    for (int i = 0; i < beatMax; i++) {
-      musicState += i;
-      ///horizontally
-      ///loop through 5 character spaces
-      for (int j = 0; j < firstline.size(); j++) {
-        ///loop through list for possible X or | or empty
-        for (int k = 0; k < model.getNotes().size(); k++) {
-          if (model.getNotes().get(k).getDur().getStartBeat() == i
-                  && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
-                  && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
-            musicState += "  X  ";
-          } else {
-            if (model.getNotes().get(k).getDur().getEndBeat() == i
-                    && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
-                    && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
-              musicState += "  |  ";
-            } else {
-              if (model.getNotes().get(k).getDur().getStartBeat() < i
-                      && model.getNotes().get(k).getDur().getEndBeat() >= i
-                      && model.getNotes().get(k).getPitch() == firstline.get(j).getPitch()
-                      && model.getNotes().get(k).getOctave() == firstline.get(j).getOctave()) {
-                musicState += "  |  ";
-              }
-            }
-          }
-        }
-        if (!(model.getNotes().contains(firstline.get(j)))) {
-          musicState += "     ";
-        }
-      }
-      musicState += "\n";
+    System.out.print(firstline);
+    for(int i = 0; i < firstline.size(); i++) {
+      g.drawString(firstline.get(i).toString(), 0,
+              (2 * NOTE_WIDTH) + NOTE_WIDTH * i);
     }
-    ///required new line at the end
-    return musicState += "\n";
   }
 
 
-//  @Override
-//  public String getState() {
-//    String output = "";
-//    Note lowestNote = this.getLowestNote();
-//    Note highestNote = this.getHighestNote();
-//    List<Note> encountered = new ArrayList<>();
-//    List<Integer> encounteredBeats = new ArrayList<>();
-//
-//    // DEALS WITH FIRST LINE -- SHOULD GENERATE
-//    // if empty, lowestNote and highestNote should return null values
-//    if (this.isEmpty()) {
-//      output.concat(this.firstLine(null, null));
-//    } else {
-//      output.concat(this.firstLine(lowestNote, highestNote));
-//      // if this branch ever returns null lowestnote and null highestNote
-//      // then I screwed up in getLowestNote() and getHighestNote();
-//    }
-//
-//    int totalBeats = this.numBeats();
-//    for (int currentBeat = 0; currentBeat < totalBeats; currentBeat += 1) {
-//      if (this.map.containsKey(currentBeat)) {
-//        for (Note tempNote : this.map.get(currentBeat)) {
-//          encountered.add(tempNote);
-//        }
-//        encountered.addAll(this.map.get(currentBeat));
-//        encounteredBeats.add(currentBeat);
-//      }
-//
-//      // the intro to every line but the first
-//      output.concat(this.getPaddedInt(currentBeat, totalBeats));
-//      output.concat(this.genPerBeat(currentBeat, lowestNote, highestNote, encountered));
-//    }
-//    return output;
-//  }
-//
-  @Override
-  public void render() {
-
+  /// added twice because of some import error
+  public int columns(int i, int max) {
+    if (i == max) {
+      return max;
+    } else {
+      return 11;
+    }
+  }
+  /**
+  * Scrolls the top half to the right when the red line touches the last note or the edge of the noteboard
+  *
+  */
+  public void scroll() {
+    JPanel container = new JPanel();
+    JScrollPane jsp = new JScrollPane(container);
+    container.setPreferredSize(new Dimension(500, 250));
+    container.setLayout(null);
   }
 }
